3308fe868554f9ae1bae10d43cc51623c0b9ce49
Sarina Burch <sarinaburch52@gmail.com>
2020-09-10 19:44:37 -0400

    created folders and main files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hamburger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheeseburger", 0);
